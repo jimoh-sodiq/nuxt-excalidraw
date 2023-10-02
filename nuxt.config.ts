dbe8d48fa765ffb6cb2022d23706ea8bf693047d
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-10-02 18:55:23 +0100

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/devtools",'@vueuse/nuxt', '@nuxt/ui', 'nuxt-icon']
-})
+  modules: ["@nuxt/devtools", "@vueuse/nuxt", "@nuxt/ui", "nuxt-icon"],
+  ssr: false,
+});
