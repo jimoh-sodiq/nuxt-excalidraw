6547bb4c0b8f002a0ca4f2f82e1192c8a57238f2
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-10-07 08:28:54 +0100

    implement drawing basic shape

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { useRefHistory, useWindowSize } from "@vueuse/core";
+import { useWindowSize } from "@vueuse/core";
 import rough from "roughjs";
 import { RoughCanvas } from "roughjs/bin/canvas";
 
 const { width, height } = useWindowSize();
-const { x, y } = useMouse();
+// const { x, y } = useMouse();
 const canvas = ref();
 
 const hexColorCode = ref("#0335fc");
@@ -19,25 +19,49 @@ onMounted(() => {
   rc.value = rough.canvas(canvas.value);
 });
 
-const startPoint = ref();
-const selectedShape = ref("rectangle");
+type ElementTypes = "rectangle" | "line";
+
+const renderedElements = ref<any>([]);
+const tempElements = ref<any>([]);
+
+watch(renderedElements, () => {
+  ctx.value?.clearRect(0, 0, canvas.value.width, canvas.value.height);
+  renderedElements.value.forEach((item: any) => rc.value?.draw(item.roughElement));
+});
+
+const startPoint = ref<{ x: number; y: number }>({ x: 0, y: 0 });
+const selectedShape = ref<ElementTypes>("line");
 const isDrawing = ref(false);
 
-function drawShape(x1: number, y1: number, x2: number, y2: number) {
+function createElement(x1: number, y1: number, x2: number, y2: number) {
   switch (selectedShape.value) {
-    case "rectangle":
-      (rc.value as RoughCanvas).circle(x1, y1, 50);
-      // (ctx.value as CanvasRenderingContext2D).clearRect(x1, y1, 30, 30);
-
-      break;
-
+    case "line":
+      const roughElement = generator.rectangle(x1, y1, x2-x1, y2-y1);
+      return {
+        x1,
+        y1,
+        x2,
+        y2,
+        roughElement,
+      };
     default:
       break;
   }
 }
 
+// function handleTouchDown(event: TouchEvent) {}
+// function handleTouchMove(event: TouchEvent) {}
+// function handleTouchUp(event: TouchEvent) {}
+
 function handleMouseDown(event: MouseEvent) {
   isDrawing.value = true;
+  const element = createElement(
+    event.clientX,
+    event.clientY,
+    event.clientX,
+    event.clientY
+  );
+  renderedElements.value.push(element);
 }
 
 function handleMouseUp(event: MouseEvent) {
@@ -46,25 +70,37 @@ function handleMouseUp(event: MouseEvent) {
 
 function handleMouseMove(event: MouseEvent) {
   // console.log(event);
-  const { clientX, clientY, movementX, movementY } = event;
+  // ctx.value?.clearRect(0, 0, canvas.value.width, canvas.value.height);
+  if (!isDrawing.value) return;
+  const index = renderedElements.value.length - 1;
+  const { x1, y1 } = renderedElements.value[index];
+  const updatedElement = createElement(x1, y1, event.clientX, event.clientY);
 
-  if (isDrawing.value) {
-    drawShape(clientX, clientY, movementX, movementY);
-  }
+  const renderedElementsCopy = [...renderedElements.value];
+  renderedElementsCopy[index] = updatedElement;
+  renderedElements.value = renderedElementsCopy;
+  console.log(renderedElements.value)
+  // console.log(x1, y1);
 }
+
+function resetCanvas() {
+  (ctx.value as CanvasRenderingContext2D).reset();
+}
+
 function handleDrag() {}
 </script>
 
 <template>
-  <div class="relative">
+  <div class="relative text-red-500">
+    <!-- <button @click="resetCanvas" class="text-red-500">Reset Canvas</button> -->
     <NavBarMain />
+    <!-- {{ tempElements }} -->
+    <!-- {{ renderedElements.length }} -->
     <canvas
       @mousemove="handleMouseMove"
       @mousedown="handleMouseDown"
       @mouseup="handleMouseUp"
       ref="canvas"
-      class=""
-      :class="canvasBackground"
       :width="width"
       :height="height"
     >
